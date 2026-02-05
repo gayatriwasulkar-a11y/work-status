0e5f0187ee8d0e8e28a637af7e7bb275eca46ebb
gayatriwasulkar-a11y <gayatriwasulkar@gmail.com>
2026-02-05 17:10:35 +0530

    hoisting.js

---
+++ hoisting.js
@@ -0,0 +1,65 @@
+1.console.log(a)
+greet()
+function greet()
+{
+    console.log("Good Morning")
+}
+
+var a=9;
+console.log(a)
+
+2.console.log(typeof test);
+
+var test = "Hello";
+function test() {}
+
+console.log(typeof test); 
+
+3.greet();
+
+function greet() {
+  console.log("Hi all of you");
+}
+
+4.console.log(a);
+var a = "Hello";
+console.log(a);
+
+5.try {
+  console.log(b); 
+} catch (e) {
+  console.log(e.name);
+}
+let b = "Hello";
+
+6.try {
+  Hi(); 
+} catch (e) {
+  console.log(e.name);
+}
+
+var Hi = function() {
+  console.log("Hello");
+};
+
+7.try {
+  shout();
+} catch (g) {
+  console.log(g.name);
+}
+const shout = () => console.log("Hello");
+
+8.{
+  console.log(a);
+  var a = 5;
+}
+console.log(a); 
+
+9.var a = 1;
+var a = 2;
+
+console.log(a);
+
+10.var a;
+console.log(a);
+a = 10;
